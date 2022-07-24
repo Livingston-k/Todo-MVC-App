7ae9d45894b9ff8b0de81f36d0a4243473045a63
Livingston-k <kaddulivington@gmail.com>
2022-07-25 02:59:42 +0300

    change

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,20 @@
-console.log('Hello wold')
+import express, { Request, Response } from 'express'
+import db from './config/database.config'
+
+const app = express()
+const port = 3000
+
+db.sync().then(() => {
+    console.log('Connect to database')
+})
+
+
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send("Hello World")
+})
+
+app.listen(port, () => {
+    console.log("App is runnning on port http://localhost:" + port)
+})

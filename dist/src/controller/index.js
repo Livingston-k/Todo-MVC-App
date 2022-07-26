ee91716d4b319c0cbc3f544493125a84f8a66d95
Livingston-k <kaddulivington@gmail.com>
2022-07-26 12:02:57 +0300

    controllers

---
+++ dist/src/controller/index.js
@@ -0,0 +1,82 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const uuid_1 = require("uuid");
+const model_1 = __importDefault(require("../model"));
+class TodoController {
+    // READ TODO WITH PAGENATIONS
+    async read(req, res) {
+        try {
+            const limit = req.query?.limit;
+            const offset = req.query?.offset;
+            const todos = await model_1.default.findAll({ limit, offset });
+            res.json(todos);
+        }
+        catch (error) {
+            res.json({ msg: "Error reading todos", status: 500 });
+        }
+    }
+    // CREATE NEW TODO
+    async create(req, res) {
+        const id = (0, uuid_1.v4)();
+        try {
+            const record = await model_1.default.create({
+                ...req.body, id
+            });
+            res.json({ record, msg: "Record successfully added" });
+        }
+        catch (error) {
+            res.json({ msg: "Error creting todo", status: 500 });
+        }
+    }
+    // TODO DETAILS
+    async show(req, res) {
+        try {
+            const { id } = req.params;
+            const todo = await model_1.default.findOne({ where: { id } });
+            res.json(todo);
+        }
+        catch (error) {
+            res.json({ msg: "Error reading todo if id ", status: 500 });
+        }
+    }
+    // UPDATE TODO
+    async update(req, res) {
+        try {
+            const { id } = req.params;
+            const todo = await model_1.default.findOne({ where: { id } });
+            if (!todo) {
+                return res.json({ msg: "No record found for the provided id", status: 404 });
+            }
+            const updatedRecord = await todo.update({
+                completed: !todo.getDataValue('completed')
+            });
+            res.json({ updatedRecord, msg: 'Todo updated successfully' });
+        }
+        catch (error) {
+            res.json({ msg: "Error reading todo if id ", status: 500 });
+        }
+    }
+    // DELETE TODO
+    async destroy(req, res) {
+        try {
+            const { id } = req.params;
+            const todo = await model_1.default.findOne({ where: { id } });
+            if (!todo) {
+                return res.json({ msg: "No record found for the provided id", status: 404 });
+            }
+            const deletedRecord = await todo.destroy();
+            res.json({ msg: 'Todo deleted successfully' });
+        }
+        catch (error) {
+            res.json({ msg: "Error deleting todo", status: 500 });
+        }
+    }
+    // WARNING ROUTE
+    async alert(req, res) {
+        res.json({ 'msg': 'Use Postman to consume the Todo api' });
+    }
+}
+exports.default = new TodoController();
--- dist/src/server.js
+++ dist/src/server.js
@@ -4,11 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const index_1 = __importDefault(require("./model/index"));
 const database_config_1 = __importDefault(require("./config/database.config"));
-const uuid_1 = require("uuid");
-const index_2 = __importDefault(require("./validator/index"));
+const index_1 = __importDefault(require("./validator/index"));
 const middleware_1 = __importDefault(require("./middleware"));
+const controller_1 = __importDefault(require("./controller"));
 const app = (0, express_1.default)();
 const port = 3000;
 app.use(express_1.default.json());
@@ -16,78 +15,13 @@ app.use(express_1.default.json());
 database_config_1.default.sync().then(() => {
     console.log('Connected to database');
 });
-// ROUTERS
-app.get('/', (req, res) => {
-    res.json({ 'msg': 'Use Postman to consume the Todo api' });
-});
-// CREATE TODO RECORD
-app.post('/create', index_2.default.checkCreateTodo(), middleware_1.default.handleValidationErrors, async (req, res) => {
-    const id = (0, uuid_1.v4)();
-    try {
-        const record = await index_1.default.create({
-            ...req.body, id
-        });
-        res.json({ record, msg: "Record successfully added" });
-    }
-    catch (error) {
-        res.json({ msg: "Error creting todo", status: 500 });
-    }
-});
-//READ TODO RECORDS
-app.get('/read', index_2.default.checkReadTodo(), middleware_1.default.handleValidationErrors, async (req, res) => {
-    try {
-        const limit = req.query?.limit;
-        const offset = req.query?.offset;
-        const todos = await index_1.default.findAll({ limit, offset });
-        res.json(todos);
-    }
-    catch (error) {
-        res.json({ msg: "Error reading todos", status: 500 });
-    }
-});
-//READ TODO RECORDS
-app.get('/read/:id', index_2.default.checkIdParam(), middleware_1.default.handleValidationErrors, async (req, res) => {
-    try {
-        const { id } = req.params;
-        const todo = await index_1.default.findOne({ where: { id } });
-        res.json(todo);
-    }
-    catch (error) {
-        res.json({ msg: "Error reading todo if id ", status: 500 });
-    }
-});
-// UPDATE TODO RECORDS
-app.put('/update/:id', index_2.default.checkIdParam(), middleware_1.default.handleValidationErrors, async (req, res) => {
-    try {
-        const { id } = req.params;
-        const todo = await index_1.default.findOne({ where: { id } });
-        if (!todo) {
-            return res.json({ msg: "No record found for the provided id", status: 404 });
-        }
-        const updatedRecord = await todo.update({
-            completed: !todo.getDataValue('completed')
-        });
-        res.json({ updatedRecord, msg: 'Todo updated successfully' });
-    }
-    catch (error) {
-        res.json({ msg: "Error reading todo if id ", status: 500 });
-    }
-});
-// Delete TODO RECORDS
-app.delete('/delete/:id', index_2.default.checkIdParam(), middleware_1.default.handleValidationErrors, async (req, res) => {
-    try {
-        const { id } = req.params;
-        const todo = await index_1.default.findOne({ where: { id } });
-        if (!todo) {
-            return res.json({ msg: "No record found for the provided id", status: 404 });
-        }
-        const deletedRecord = await todo.destroy();
-        res.json({ msg: 'Todo deleted successfully' });
-    }
-    catch (error) {
-        res.json({ msg: "Error deleting todo", status: 500 });
-    }
-});
+//TODO ROUTES
+app.get('/', controller_1.default.alert);
+app.post('/create', index_1.default.checkCreateTodo(), middleware_1.default.handleValidationErrors, controller_1.default.create);
+app.get('/read', index_1.default.checkReadTodo(), middleware_1.default.handleValidationErrors, controller_1.default.read);
+app.get('/show/:id', index_1.default.checkIdParam(), middleware_1.default.handleValidationErrors, controller_1.default.show);
+app.put('/update/:id', index_1.default.checkIdParam(), middleware_1.default.handleValidationErrors, controller_1.default.update);
+app.delete('/delete/:id', index_1.default.checkIdParam(), middleware_1.default.handleValidationErrors, controller_1.default.destroy);
 // SERVER 
 app.listen(port, () => {
     console.log("App is runnning on port http://localhost:" + port);
